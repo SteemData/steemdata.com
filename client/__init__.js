caf751f33efafd38653c53e05eaa5eeec107ad19
furion <_@furion.me>
2016-12-28 05:48:20 +0100

    improved footer/header

--- client/__init__.js
+++ client/__init__.js
@@ -8,3 +8,15 @@ import '../imports/ui/mainLayout/layout.html'
 import '../imports/ui/home/home.js'
 import '../imports/ui/stats/stats.js'
 
+// set page metadata
+let title = "SteemData";
+DocHead.setTitle(title);
+
+let metaInfo = {name: "description", content: "SteemData offers a public database layer for STEEM blockchain."};
+DocHead.addMeta(metaInfo);
+
+let metaViewport = {name: "viewport", content: "width=device-width, initial-scale=1", };
+DocHead.addMeta(metaViewport);
+
+let linkInfo = {rel: "icon", type: "image/png", href: "/favicon.png"};
+DocHead.addLink(linkInfo);
